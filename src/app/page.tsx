ae58e70165395c3be1a0d9f6fb712eceb3154bba
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-05-04 12:14:51 -0500

    Shadcn-ui added chore: Add utility function for merging CSS classes
    This commit adds a utility function `cn` to merge CSS classes using the `clsx` and `twMerge` libraries. This function improves the readability and maintainability of the code by providing a centralized way to combine multiple class values.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
+import { Button } from "~/components/ui/button";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -55,6 +56,8 @@ export default async function Home() {
             </Link>
           </div>
         </div>
+
+        <Button>This is a button</Button>
 
         <CrudShowcase />
       </div>
