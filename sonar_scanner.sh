542f121c7daf6b9b8aa93a26491e0e5ecc3d5c6d
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-05-04 22:44:07 -0500

    chore: Update Docker configuration for sonar_scanner.sh
    Update Docker configuration for the `sonar_scanner.sh` script. Set necessary environment variables for SonarQube scanning, including the SonarQube URL, project key, and authentication token. Also, update the volume mapping to use the current directory as the source directory. These changes improve the SonarQube scanning process and ensure the correct setup of the Sonar Scanner CLI.

--- sonar_scanner.sh
+++ sonar_scanner.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+# Load the .env file
+if [ -f ".env" ]; then
+    export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+echo "Running Sonar Scanner"
+echo "SonarQube URL: ${SONARQUBE_URL}"
+echo "Project Key: ${YOUR_PROJECT_KEY}"
+echo "Sonar Token: ${SONAR_TOKEN}"
+echo "Current Directory: ${PWD}"
+
 docker run \
     --rm \
-    -e SONAR_HOST_URL="http://${SONARQUBE_URL}" \
+    -e SONAR_HOST_URL="${SONARQUBE_URL}" \
     -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=${YOUR_PROJECT_KEY}" \
-    -e SONAR_TOKEN="myAuthenticationToken" \
+    -e SONAR_TOKEN=${SONAR_TOKEN} \
     -v "${PWD}:/usr/src" \
     sonarsource/sonar-scanner-cli
